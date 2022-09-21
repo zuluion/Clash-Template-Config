912d79aac7686c8225c17ea14e54d3cc61482ca8
Evan Lee <873999785@qq.com>
2022-09-21 16:24:17 +0800

    modify ini ps script
    1. rename it if ".\myprovider" is exist 
    2. create  ".\myprovider\proxies\provider-airport.yml" from the current project 
    3. clash install current template configuration file
    **ps: This script is used for powershell remote call execution**

--- Script/ini-clash-provider.ps1
+++ Script/ini-clash-provider.ps1
@@ -3,9 +3,19 @@ $clash_dir = "$env:USERPROFILE"+"\.config\clash"
 $dir_isExist = test-Path -Path $clash_dir
 if($dir_isExist){
     cd $clash_dir 
-    powershell mkdir -p .\myprovider\proxies,.\myprovider\ruleset
+    if(!".\myprovider"){
+        powershell mkdir -p .\myprovider\proxies,.\myprovider\ruleset
+    } else {
+        $folder_time_str = ".\myprovider-backup-"+(Get-Date).tostring("yyyy-MM-dd-HH-mm-ss")
+        powershell Rename-Item .\myprovider $folder_time_str
+        powershell mkdir -p .\myprovider\proxies,.\myprovider\ruleset
+    }   
     powershell cd .\myprovider\proxies
-    powershell New-Item -Path .\myprovider\proxies -ItemType file -Name provider-airport.yml
-    powershell New-Item -Path .\myprovider\proxies -ItemType file -Name provider-paoche.yml
-    powershell start $clash_dir 
+    # powershell New-Item -Path .\myprovider\proxies -ItemType file -Name provider-airport.yml
+    # powershell New-Item -Path .\myprovider\proxies -ItemType file -Name provider-paoche.yml
+    powershell iwr -Uri "https://cdn.jsdelivr.net/gh/yeehonglee/Clash-Template-Config@master/Ini-Files/provider-airport.yml" -OutFile ".\myprovider\proxies\provider-airport.yml"
+    # powershell iwr -Uri "https://cdn.jsdelivr.net/gh/yeehonglee/Clash-Template-Config@master/Ini-Files/provider-paoche.yml" -OutFile ".\myprovider\proxies\provider-paoche.yml"
+    start "clash://install-config?url=https://cdn.jsdelivr.net/gh/yeehonglee/Clash-Template-Config@master/Clash-Template-Config.yml"
+    # powershell start $clash_dir 
+    echo "初始化完成"
 }
